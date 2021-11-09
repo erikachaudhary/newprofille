ba65961753263d2b0066332172a8b2fa3668fdeb
Erika Chaudhary <erikap02@gmail.com>
2021-11-09 12:14:19 +0100

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[ about blog services contact]
 
   def home
   end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def services
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'services', to: 'pages#services'
+  get 'blog', to: 'pages#blog'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
